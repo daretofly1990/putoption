8e480a2b23620f5abaa1c9ac65272770961841fe
daretofly1990 <seven_dragon_22@yahoo.com>
2026-04-14 22:07:54 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,10 +6,13 @@ const ASSETS = [
   '/putoption/icons/icon-512.png'
 ];
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
   self.skipWaiting();
 });
-self.addEventListener('activate', e => { self.clients.claim(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request).catch(() => caches.match('/putoption/index.html'))));
 });
